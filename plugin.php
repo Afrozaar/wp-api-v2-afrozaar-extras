5d2c1f81caec1662d3070882a90601614cad4229
Jan-Louis Crafford <jlocraf@gmail.com>
2016-01-11 15:50:03 +0200

    Added check for WP REST API on plugin activate

--- plugin.php
+++ plugin.php
@@ -11,6 +11,13 @@ Author: Jan-Louis Crafford, Afrozaar Consulting
 
 */
 
+include_once( ABSPATH . 'wp-admin/includes/plugin.php');
+
+if (!is_plugin_active('rest-api/plugin.php')) {
+  add_action('admin_notices', 'pim_draw_notice_rest_api');
+  return;
+}
+
   require_once dirname( __FILE__ ) . '/lib/endpoints/class-wp-rest-users-extras-controller.php';
 
   require_once dirname( __FILE__ ) . '/lib/endpoints/class-wp-rest-media-extras-controller.php';
@@ -34,5 +41,15 @@ Author: Jan-Louis Crafford, Afrozaar Consulting
   $allowedposttags['script'] = array(
 			'type' => array (),
 		);
+
+
+    function pim_draw_notice_rest_api() {
+      echo '<div id="message" class="error fade"><p style="line-height: 150%">';
+
+    _e('<strong>WP REST API - Afrozaar Extras</strong></a> requires the WP REST API plugin to be activated. Please <a href="http://wordpress.org/plugins/rest-api/">install / activate WP REST API</a> first.', 'rest-api');
+
+    echo '</p></div>';
+
+    }
 
  ?>
