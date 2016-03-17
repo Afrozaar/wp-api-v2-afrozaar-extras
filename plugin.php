a7e728d34e4a392956c8387643bd1d720778162d
Jan-Louis Crafford <jlocraf@gmail.com>
2016-03-17 16:32:43 +0200

    added new REST endpoint to fetch user id using email address

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
   /*
   * Plugin Name: WP REST Api Extras
   * Description:  Adds extra WP REST Api calls
-  * Version:  0.5
+  * Version:  0.6
   * Author: Jan-Louis Crafford, Afrozaar Consulting
   * Plugin URI: https://github.com/Afrozaar/wp-api-v2-afrozaar-extras
   */
